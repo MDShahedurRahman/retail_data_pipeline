8a88e0a57bf4884264307d2e67b461db17b80257
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-09 20:50:01 -0500

    add dropDuplicates logic

--- jobs/silver_cleaning.py
+++ jobs/silver_cleaning.py
@@ -3,7 +3,4 @@ from pyspark.sql.functions import col, to_date
 
 def clean_sales_data(df, silver_path):
 
-    cleaned_df = df.dropDuplicates() \
-        .dropna() \
-        .withColumn("order_date", to_date(col("order_date"))) \
-        .withColumn("total_price", col("quantity") * col("unit_price"))
+    cleaned_df = df.dropDuplicates()
