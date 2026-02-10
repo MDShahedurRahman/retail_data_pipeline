1284c68fe78d7e8748fe654def4fd65b8cae9839
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-09 19:25:14 -0500

    Add some print() functions

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ from jobs.business_queries import top_categories, top_customers, revenue_by_city
 
 def main():
     spark = get_spark_session()
+
+    print("Running Bronze Job...")
+    bronze_df = ingest_raw_sales(spark, RAW_FILE, BRONZE_PATH)
+
+    print("Running Silver Job...")
+    silver_df = clean_sales_data(bronze_df, SILVER_PATH)
+
+    print("Running Gold Job...")
+    build_star_schema(silver_df, GOLD_PATH)
+
+    print("\nTop Categories Revenue:")
+    top_categories(silver_df).show()
+
+    print("\nTop Customers Spending:")
+    top_customers(silver_df).show()
+
+    print("\nRevenue by City:")
+    revenue_by_city(silver_df).show()
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
