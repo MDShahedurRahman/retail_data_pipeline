ca7b2f67ab2e692dadc2995d6dd45ab94e4a4f07
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-09 21:19:03 -0500

    implement dim_product creation

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -1,3 +1,5 @@
 def build_star_schema(df, gold_path):
 
     dim_customer = df.select("customer_id", "customer_name", "city").distinct()
+    dim_product = df.select(
+        "product_id", "product_name", "category").distinct()
