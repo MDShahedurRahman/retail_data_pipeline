728b8f871ff24412b05569190dd8fd22cf5b5766
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-09 21:21:47 -0500

    save dim_product parquet tables

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -15,3 +15,4 @@ def build_star_schema(df, gold_path):
     )
 
     dim_customer.write.mode("overwrite").parquet(gold_path + "/dim_customer")
+    dim_product.write.mode("overwrite").parquet(gold_path + "/dim_product")
