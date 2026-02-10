f421b9cc62d3a6198b82297661bfc64f90bf82d6
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-09 21:08:16 -0500

    write silver parquet output

--- jobs/silver_cleaning.py
+++ jobs/silver_cleaning.py
@@ -7,3 +7,6 @@ def clean_sales_data(df, silver_path):
         .dropna() \
         .withColumn("order_date", to_date(col("order_date"))) \
         .withColumn("total_price", col("quantity") * col("unit_price"))
+
+    cleaned_df.write.mode("overwrite").parquet(silver_path)
+    return cleaned_df
