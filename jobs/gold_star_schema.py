1706edbf2e9ceecb327362483f8556a41262269b
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-09 21:19:33 -0500

    implement fact_sales creation

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -3,3 +3,13 @@ def build_star_schema(df, gold_path):
     dim_customer = df.select("customer_id", "customer_name", "city").distinct()
     dim_product = df.select(
         "product_id", "product_name", "category").distinct()
+
+    fact_sales = df.select(
+        "order_id",
+        "customer_id",
+        "product_id",
+        "order_date",
+        "quantity",
+        "unit_price",
+        "total_price"
+    )
